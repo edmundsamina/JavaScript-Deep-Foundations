bdd0a62161e9fdad2f07b0a33dfe11fa04425e3c
edmundsamina <edmundsamina@gmail.com>
2023-05-14 12:57:14 +0100

    equality excercise completed

--- types-exercises/equality/ex.js
+++ types-exercises/equality/ex.js
@@ -1,6 +1,44 @@
 // TODO: write `findAll(..)`
 
+function findAll(matchValue, array){
+	//initalise empty array to push all the matching values into, before returning
+const returnArray = []
+//loop though the array checking each value against the matchValue.
+for(let i = 0; i < array.length; i++){
+			//first check which values are equal WITHOUT coercion, if they match without coersion they would be the same value.
+	if(Object.is(matchValue, array[i])){
+		//if this if statement resolves to be true, then the values do match and it can be pushed to the returnArray
+		returnArray.push(array[i])
+		//next check which values and be coerced into matching, the array should also include all coersice matches
 
+		//checl if both the types are booleans first (booleans can only match booleans in the excercise instructions)
+	} else if(typeof matchValue == 'boolean' &&  typeof array[i] == 'boolean'){
+		//if they are both booleans check if they are equal to each other, we can use the double == because we know both the types are the same
+		if(matchValue == array[i]){
+			returnArray.push(array[i])
+
+		}
+		//checking for string matches
+	} else if (typeof matchValue == 'string' &&  matchValue.trim() !== '' && typeof array[i] == 'number' && !Object.is(array[i], -0)){
+		if(matchValue == array[i])
+		{
+			returnArray.push(array[i])
+		}
+
+	} else if (typeof matchValue == 'number' && matchValue !== NaN && matchValue !== Infinity && matchValue !== -Infinity && typeof array[i] == 'string' && array[i].trim() !== '' && !Object.is(matchValue, -0)){
+		if(matchValue == array[i])
+		{
+			returnArray.push(array[i])
+		}
+
+	} else if (matchValue == null  && array[i] == undefined ){
+		returnArray.push(array[i])
+
+	}
+}
+
+return returnArray
+}
 
 // tests:
 var myObj = { a: 2 };
